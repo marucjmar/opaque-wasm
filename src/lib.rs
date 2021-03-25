c70433e2f45c8214ebdfa410129be2ca77e69e9c
Lewis Rockliffe <r0kk3rz@gmail.com>
2021-03-25 11:37:25 +1100

    fix hash function to Scrypt

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ impl CipherSuite for Default {
     type Group = curve25519_dalek::ristretto::RistrettoPoint;
     type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDH;
     type Hash = sha2::Sha512;
-    type SlowHash = opaque_ke::slow_hash::NoOpHash;
+    type SlowHash = Scrypt;
 }
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global

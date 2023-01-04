535a97f2b96b574691b2499170a485c3a0fea22c
Aistis Raulinaitis <sheganinans@gmail.com>
2023-01-04 00:50:27 -0800

    use argon

--- src/hash_methods.rs
+++ src/hash_methods.rs
@@ -2,8 +2,8 @@ use opaque_ke::ciphersuite::CipherSuite;
 
 pub struct Default;
 impl CipherSuite for Default {
-    type OprfCs = opaque_ke::Ristretto255;
-    type KeGroup = opaque_ke::Ristretto255;
+    type OprfCs = p256::NistP256;
+    type KeGroup = p256::NistP256;
     type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDh;
-    type Ksf = opaque_ke::ksf::Identity;
+    type Ksf = argon2::Argon2<'static>;
 }

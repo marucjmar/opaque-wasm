53a0453d0f9c85e0dc03a5b77f7b77a10f7ef18d
Francois Best <github@francoisbest.com>
2023-01-28 07:07:54 +0100

    feat: Allow building separate client/server packages
    It saves a few KB on the client bundle when:
    - not including the server handling code
    - stripping debug symbols in release mode
    - using wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
+#[cfg(feature = "client")]
 pub mod client_login;
+#[cfg(feature = "client")]
 pub mod client_registration;
+#[cfg(feature = "server")]
 pub mod handle_login;
+#[cfg(feature = "server")]
 pub mod handle_registration;
+#[cfg(feature = "server")]
 pub mod server_setup;
+
 mod hash_methods;
 mod utils;
 
@@ -12,8 +18,13 @@ mod utils;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[cfg(feature = "client")]
 pub use client_login::Login;
+#[cfg(feature = "client")]
 pub use client_registration::Registration;
+#[cfg(feature = "server")]
 pub use handle_login::HandleLogin;
+#[cfg(feature = "server")]
 pub use handle_registration::HandleRegistration;
+#[cfg(feature = "server")]
 pub use server_setup::ServerSetup;
